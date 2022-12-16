924f5ac529d72b819bd73989d75fd28d9c1e9212
JeffersonAlves7 <ilj031420@gmail.com>
2022-12-16 09:29:56 -0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      purple: '#9039FF',
+      blue: '#0077B7'
+    }
+  },
+  plugins: []
+}
